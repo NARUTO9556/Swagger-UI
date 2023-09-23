31c4ad910ce1c440df268b4e503ec070036e4467
NARUTO9556 <gruver5263@gmail.com>
2023-09-23 21:14:25 +0800

    дз по swagger

---
+++ src/main/java/ru/hogwarts/school/controller/FacultyController.java
@@ -0,0 +1,40 @@
+package ru.hogwarts.school.controller;
+
+import org.springframework.web.bind.annotation.*;
+import ru.hogwarts.school.model.Faculty;
+import ru.hogwarts.school.service.FacultyService;
+
+import java.util.List;
+@RestController
+@RequestMapping("/faculties")
+public class FacultyController {
+    private final FacultyService facultyService;
+
+    public FacultyController(FacultyService facultyService) {
+        this.facultyService = facultyService;
+    }
+
+    @GetMapping("{id}")
+    public Faculty get(@PathVariable Long id) {
+        return facultyService.get(id);
+    }
+    @PostMapping
+    public Faculty add(@RequestBody Faculty faculty) {
+        return facultyService.add(faculty);
+    }
+
+    @PutMapping("{id}")
+    public Faculty update(@PathVariable Long id, @RequestBody Faculty faculty) {
+        return facultyService.update(id, faculty);
+    }
+
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable Long id) {
+        facultyService.delete(id);
+    }
+
+    @GetMapping
+    public List<Faculty> getByAge(@RequestParam("color") String color) {
+        return facultyService.getByColor(color);
+    }
+}
