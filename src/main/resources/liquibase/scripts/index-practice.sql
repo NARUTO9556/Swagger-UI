bc7185017125bded8e1261651c6e3c5aab0426b5
NARUTO9556 <gruver5263@gmail.com>
2023-10-25 01:16:17 +0800

    4.3 миграция и индексы

---
+++ src/main/resources/liquibase/scripts/index-practice.sql
@@ -0,0 +1,7 @@
+-- liquibase formatter sql
+
+--changeset vaverin:1
+CREATE INDEX student_name_index ON student (name);
+
+--changeset vaverin:2
+CREATE INDEX faculty_nc_index ON faculty (name, color);
